0bfd3676bb4dbd46b1b7c5166369b4e9d5fd45f4
Daniel <daniellaguado8@hotmail.com>
2025-06-26 12:51:22 -0300

    Actualizo app de vocabulario con boton speech to tex 1.8.3

--- src/App.js
+++ src/App.js
@@ -752,35 +752,59 @@ const App = () => {
                       </button>
                       <div className='category-actions'>
                         <button
-                          onClick={() =>
-                            navigateToAddCard(cat.id)
-                          } /* Nuevo botón para añadir tarjetas */
+                          onClick={() => navigateToAddCard(cat.id)}
                           className='button add-item-button'
                           disabled={isLoading}
+                          aria-label='Agregar Tarjeta'
                         >
-                          Agregar
-                        </button>
-                        {/* Nuevo botón para iniciar Quiz */}
-                        <button
-                          onClick={() => navigateToQuizPage(cat.id)}
-                          className='button quiz-button'
-                          disabled={isLoading}
-                        >
-                          Quiz
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='100%'
+                            height='100%'
+                            fill='currentColor'
+                            viewBox='0 0 16 16'
+                          >
+                            {" "}
+                            <path
+                              fillRule='evenodd'
+                              d='M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2'
+                            />
+                          </svg>
                         </button>
                         <button
                           onClick={() => startEditCategory(cat)}
                           className='button edit-button'
                           disabled={isLoading}
+                          aria-label='Editar Categoría'
                         >
-                          Editar
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='100%'
+                            height='100%'
+                            fill='currentColor'
+                            viewBox='0 0 16 16'
+                          >
+                            {" "}
+                            <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325' />
+                          </svg>
                         </button>
                         <button
                           onClick={() => confirmDeleteCategory(cat.id)}
                           className='button delete-button'
                           disabled={isLoading}
+                          aria-label='Eliminar Categoría'
                         >
-                          Eliminar
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='100%'
+                            height='100%'
+                            fill='currentColor'
+                            viewBox='0 0 16 16'
+                          >
+                            {" "}
+                            <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z' />{" "}
+                            <path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z' />
+                          </svg>
                         </button>
                       </div>
                     </>
