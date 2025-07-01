8180c60ef5847b9a9a5b7af387b3eeb250bd7748
Daniel <daniellaguado8@hotmail.com>
2025-07-01 08:31:03 -0300

    Introduciedo Chatbot  3.0.11

--- src/lesson/lessonCard.js
+++ src/lesson/lessonCard.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useContext } from "react";
 import "./PrincipalPageLessons.css"; // Estilos compartidos para lecciones
 import { normalizeText, renderClickableText } from "../utils/textUtils"; // Subir dos niveles para utils
 import ExerciseDisplay from "./components/ExerciseDisplay"; // ¡CORREGIDO! Ahora en la misma carpeta
-import ExerciseNavigation from "./ExerciseNavigation"; // ¡CORREGIDO! Ahora en la misma carpeta
+import ExerciseNavigation from "./components/ExerciseNavigation"; // ¡CORREGIDO! Ahora en la misma carpeta
 import PracticeChatInterface from "../Practice/components/PracticeChatInterface"; // ¡CORREGIDO! Subir dos niveles para Practice
 
 // Importar el contexto
