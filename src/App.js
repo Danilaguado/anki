cb1fc9681e266a8b0c3aa1c73882496a3924e64c
Daniel <daniellaguado8@hotmail.com>
2025-10-12 04:28:41 -0300

    icon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 // src/App.js
-import React, { useState, useRef } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/PaymentForm.css";
-import Landing from "./pages/Landing";
+import CodigoConexion from "./pages/CodigoConexion";
+import MusculoVoluntad from "./pages/MusculoVoluntad";
 import PaymentPage from "./pages/PaymentPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Landing />} />
+        <Route path='/codigo-conexion' element={<CodigoConexion />} />
+        <Route path='/musculo-voluntad' element={<MusculoVoluntad />} />
         <Route path='/payment' element={<PaymentPage />} />
       </Routes>
     </BrowserRouter>
