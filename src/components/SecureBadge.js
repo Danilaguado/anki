cd369f4421ced7c3e37d96779bc26f228419a005
Daniel <daniellaguado8@hotmail.com>
2025-10-16 20:31:47 -0300

    icon

--- src/components/SecureBadge.js
+++ src/components/SecureBadge.js
@@ -5,7 +5,6 @@ const SecureBadge = () => {
   return (
     <div className='secure-badge'>
       <img src='/assets/pago-seguro.png' alt='Pago Seguro' />
-      <span className='secure-text'>Transacción 100% Segura</span>
     </div>
   );
 };
