893429138b644869af090c703281feb2678fcd50
Daniel <daniellaguado8@hotmail.com>
2025-10-16 16:46:50 -0300

    icon

--- src/components/MembershipBanner.js
+++ src/components/MembershipBanner.js
@@ -2,32 +2,46 @@ import React from "react";
 import "../styles/MembershipBanner.css";
 
 const MembershipBanner = () => {
-  // En el futuro, puedes hacer que este enlace lleve a una página de membresía
   const handleSaberMasClick = () => {
     console.log(
       'Botón "Saber más" clickeado. Redirigir a la página de membresía.'
     );
-    // Ejemplo: navigate('/membresia');
+    // Cuando tengas la página de membresía, puedes usar: navigate('/membresia');
   };
 
   return (
-    <section className='membership-banner-section'>
-      <div className='membership-banner-content'>
-        <div className='membership-banner-text'>
-          <h3>ÚNETE A LA MEMBRESÍA MENSUAL</h3>
+    <div className='membership-banner-container'>
+      <div className='membership-banner'>
+        <div className='membership-icon'>
+          {/* Un ícono simple para darle un toque visual */}
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path d='M12 2.69l5.66 5.66a8 8 0 1 1-11.31 0L12 2.69z' />
+            <path d='M12 12l5.66-5.66' />
+            <path d='M12 12L6.34 6.34' />
+          </svg>
+        </div>
+        <div className='membership-text'>
+          <h3>Únete a la Membresía Kaizen</h3>
           <p>
             Accede a contenido exclusivo, talleres en vivo y una comunidad de
             alto valor.
           </p>
         </div>
-        <button
-          onClick={handleSaberMasClick}
-          className='membership-banner-button'
-        >
+        <button onClick={handleSaberMasClick} className='membership-button'>
           Saber más
         </button>
       </div>
-    </section>
+    </div>
   );
 };
 
